fb68441e3a408aa79e0746c8ae88b4d166e74767
SukkoPera <software@sukkology.net>
2015-01-10 17:46:22 +0100

    Fixed typo.

--- cmake/GSettings.cmake
+++ cmake/GSettings.cmake
@@ -13,7 +13,6 @@ if(GSETTINGS_COMPILE)
 endif()
 
 macro(add_schema SCHEMA_NAME)
-
     set(PKG_CONFIG_EXECUTABLE pkg-config)
     # Have an option to not install the schema into where GLib is
     if (GSETTINGS_LOCALINSTALL)
@@ -24,8 +23,8 @@ macro(add_schema SCHEMA_NAME)
     endif (GSETTINGS_LOCALINSTALL)
 
     # Run the validator and error if it fails
-    execute_process (COMMAND ${PKG_CONFIG_EXECUTABLE} gio-2.0 --variable glib_compile_schemas  OUTPUT_VARIABLE _glib_comple_schemas OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process (COMMAND ${_glib_comple_schemas} --dry-run --schema-file=${CMAKE_CURRENT_SOURCE_DIR}/${SCHEMA_NAME} ERROR_VARIABLE _schemas_invalid OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process (COMMAND ${PKG_CONFIG_EXECUTABLE} gio-2.0 --variable glib_compile_schemas  OUTPUT_VARIABLE _glib_compile_schemas OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process (COMMAND ${_glib_compile_schemas} --dry-run --schema-file=${CMAKE_CURRENT_SOURCE_DIR}/${SCHEMA_NAME} ERROR_VARIABLE _schemas_invalid OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     if (_schemas_invalid)
       message (SEND_ERROR "Schema validation error: ${_schemas_invalid}")
@@ -37,6 +36,6 @@ macro(add_schema SCHEMA_NAME)
 
     if (GSETTINGS_COMPILE)
         install (CODE "message (STATUS \"Compiling GSettings schemas\")")
-        install (CODE "execute_process (COMMAND ${_glib_comple_schemas} ${GSETTINGS_DIR})")
+        install (CODE "execute_process (COMMAND ${_glib_compile_schemas} ${GSETTINGS_DIR})")
     endif ()
 endmacro()
